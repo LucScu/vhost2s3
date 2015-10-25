ee1f89a9a8dcacaea63cf0a5187a5e87907d2ab2
EC2 Default User <ec2-user@ip-10-0-0-232.eu-west-1.compute.internal>
2015-10-25 17:24:27 +0100

    dynamic loop in vhost dir

--- backup.sh
+++ backup.sh
@@ -1,27 +1,53 @@
-# Get config file
-source config-demo
+#!/bin/bash
+PATH=/bin:/usr/bin:/usr/local/bin
 
-# move to vhost directory
-cd $vhostdir
+
+# Get config file
+currentdir=$(dirname $0)
+source $currentdir'/config'
+
 
 # create tmp dir if not exists
 mkdir -p $tmpdir
 
+
+# move to vhost dir
+cd $vhostdir
 now=$(date +"%Y%m%d")
-sourcezip=dir-to-backup
-destzip=$tmpdir$sourcezip$now'.tar.gz'
 
-echo sourcezip: $sourcezip
-echo destzip: $destzip
+
+#print $excludevhost content
+#for v in "${excludevhost[@]}"
+#do
+#  echo $v
+#done
+
+
+#loop vhost dir
+#backup only dir that aren't in $excludevhost
+for D in `ls`
+do
+  FOUND=`echo ${excludevhost[@]} | grep '$D'`
+  if [ '${FOUND}' != '' ]; then
+    echo not backup $D
+  else
+    echo backup $D
+    sourcezip=$D
+    destzip=$tmpdir$sourcezip$now'.tar.gz'
+
+    echo sourcezip: $sourcezip
+    echo destzip: $destzip
  
-tar -zcf $destzip $sourcezip
+    tar -zcf $destzip $sourcezip
 
-sources3=$destzip
-dests3='s3://'$bucket'/'$s3dir'/'$sourcezip$now'.tar.gz'
+    sources3=$destzip
+    dests3='s3://'$bucket'/'$s3dir'/'$sourcezip$now'.tar.gz'
 
-echo sources3: $sources3
-echo dests3: $dests3
+    echo sources3: $sources3
+    echo dests3: $dests3
 
-s3cmd put $sources3 $dests3
+    s3cmd put $sources3 $dests3
+  fi
+done
 
 exit 0
