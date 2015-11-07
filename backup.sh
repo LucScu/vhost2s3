6b9e3c0dd1b1b7d1fc773325cc5075c59f81180e
Luca Sculco <sculco.luca@gmail.com>
2015-11-07 16:02:21 +0100

    delete file older x days

--- backup.sh
+++ backup.sh
@@ -38,6 +38,7 @@ do
     echo sourcezip: $sourcezip
     echo destzip: $destzip
  
+    #create zip archive
     tar -zcf $destzip $sourcezip
 
     sources3=$destzip
@@ -46,7 +47,37 @@ do
     echo sources3: $sources3
     echo dests3: $dests3
 
+    #put to s3
     s3cmd put $sources3 $dests3
+
+    #remove from/tmp/s3backup
+    rm $destzip
+  fi
+done
+
+#delete old archive
+date=""
+now=`date --date="now" +%s`
+deleteOlderThan=$deleteBeforeDay*86400
+for E in `s3cmd ls s3://$bucket/$s3dir/`
+do
+  
+  #find archive date
+  DATE=`echo $E | grep "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}"`
+  if [ "${DATE}" != "" ]; then
+    date=$DATE
+  fi
+
+  #find archive filename
+  FILE=`echo $E | grep "s3.*\.tar\.gz"`
+  if [ "${FILE}" != "" ]; then
+    entry_time=`date --date="$date" +%s`
+    diff=$(($now - $deleteOlderThan))
+    if [ $diff -gt $entry_time ]; then
+      #echo $diff
+      #echo $entry_time
+      s3cmd del $FILE
+    fi
   fi
 done
 
