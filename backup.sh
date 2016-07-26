d7191961f58b48feb30f68ce3f607f72b24649a7
Luca Sculco <sculco.luca@gmail.com>
2016-07-27 01:26:39 +0200

    aggiunta opzione di backup di singola cartella
    script adattato per funzionare anche in server senza virtual hosts,
    dove c'e' la necessita' di fare il backup della document root.
    e' stata quindi aggiunta la variabile di configurazione $singledir che
    se valorizzata permette di eseguire il backup della singola cartella.

--- backup.sh
+++ backup.sh
@@ -2,73 +2,112 @@
 PATH=/bin:/usr/bin:/usr/local/bin
 
 
-# Get config file
+# get vars from config file
 currentdir=$(dirname $0)
 source $currentdir'/config'
+
+
+# other vars
+now='_'$(date +"%Y%m%d")
 
 
 # create tmp dir if not exists
 mkdir -p $tmpdir
 
 
-# move to vhost dir
-cd $vhostdir
-now=$(date +"%Y%m%d")
+# check if singledir is setted
+if [ -n "$singledir" ]; then
+  
+  # move to singledir
+  cd $singledir  
+
+  singledirname=${PWD##*/}
+  sourcezip=$singledir
+  destzip=$tmpdir$singledirname$now'.tar.gz'
+
+  echo sourcezip: $sourcezip
+  echo destzip: $destzip
+
+  # create zip archive to upload
+  tar -zcf $destzip $sourcezip
+
+  sources3=$destzip
+  dests3='s3://'$bucket'/'$s3dir'/'$singledirname$now'.tar.gz'
+
+  echo sources3: $sources3
+  echo dests3: $dests3
+
+  # put to s3
+  s3cmd put $sources3 $dests3
+
+  # remove from /tmp/s3backup
+  rm $destzip
+fi
 
 
-#print $excludevhost content
-#for v in "${excludevhost[*]}"
-#do
-#  echo $v
-#done
+
+# check if vhostdir is setted
+if [ -n "$vhostdir" ]; then
+
+  # move to vhostdir
+  cd $vhostdir
 
 
-#loop vhost dir
-#backup only dir that aren't in $excludevhost
-for D in `ls`
-do
-  FOUND=`echo ${excludevhost[*]} | grep $D`
-  if [ "${FOUND}" != "" ]; then
-    echo not backup $D
-  else
-    echo backup $D
-    sourcezip=$D
-    destzip=$tmpdir$sourcezip$now'.tar.gz'
+  # print $excludevhost content
+  #for v in "${excludevhost[*]}"
+  #do
+  #  echo $v
+  #done
 
-    echo sourcezip: $sourcezip
-    echo destzip: $destzip
+
+  #loop vhost dir
+  #backup only dir that aren't in $excludevhost
+  for D in `ls`
+  do
+    FOUND=`echo ${excludevhost[*]} | grep $D`
+    if [ "${FOUND}" != "" ]; then
+      echo not backup $D
+    else
+      echo backup $D
+      sourcezip=$D
+      destzip=$tmpdir$sourcezip$now'.tar.gz'
+
+      echo sourcezip: $sourcezip
+      echo destzip: $destzip
  
-    #create zip archive
-    tar -zcf $destzip $sourcezip
+      # create zip archive
+      tar -zcf $destzip $sourcezip
 
-    sources3=$destzip
-    dests3='s3://'$bucket'/'$s3dir'/'$sourcezip$now'.tar.gz'
+      sources3=$destzip
+      dests3='s3://'$bucket'/'$s3dir'/'$sourcezip$now'.tar.gz'
 
-    echo sources3: $sources3
-    echo dests3: $dests3
+      echo sources3: $sources3
+      echo dests3: $dests3
 
-    #put to s3
-    s3cmd put $sources3 $dests3
+      # put to s3
+      s3cmd put $sources3 $dests3
 
-    #remove from/tmp/s3backup
-    rm $destzip
-  fi
-done
+      # remove from /tmp/s3backup
+      rm $destzip
+    fi
+  done
+fi
 
-#delete old archive
+
+# delete old archive
 date=""
 now=`date --date="now" +%s`
 deleteOlderThan=$deleteBeforeDay*86400
 for E in `s3cmd ls s3://$bucket/$s3dir/`
 do
   
-  #find archive date
+  # find archive date
   DATE=`echo $E | grep "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}"`
   if [ "${DATE}" != "" ]; then
     date=$DATE
   fi
 
-  #find archive filename
+  # find archive filename
   FILE=`echo $E | grep "s3.*\.tar\.gz"`
   if [ "${FILE}" != "" ]; then
     entry_time=`date --date="$date" +%s`
